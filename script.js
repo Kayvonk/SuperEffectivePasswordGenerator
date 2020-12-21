1fa10c717000c23e05ccbba0bea9c88579250a85
kayvonk <kayvonk@gmail.com>
2020-12-21 15:04:23 -0700

    added empty string return in the case that character length criteria is not met twice

--- script.js
+++ script.js
@@ -26,17 +26,19 @@ function generatePassword() {
   if (length < 8 || length > 128 || isNaN(length)) {
     alert("Please enter a number between 8 and 128")
     var length = prompt("How many characters do you want your password to be? (minimum length: 8, maximum length: 128)");
-  }
-  var symbolconfirm = confirm("Do you want to allow special characters?");
+    if (length < 8 || length > 128 || isNaN(length)) {
+      return ""
+    }
+    var symbolconfirm = confirm("Do you want to allow special characters?");
 
-  var uppercaseconfirm = confirm("Do you want to allow uppercase letters?");
+    var uppercaseconfirm = confirm("Do you want to allow uppercase letters?");
 
-  var lowercaseconfirm = confirm("Do you want to allow lowercase letters?");
+    var lowercaseconfirm = confirm("Do you want to allow lowercase letters?");
 
-  var numbersconfirm = confirm("Do you want to allow numbers?");
-  if (!symbolconfirm && !uppercaseconfirm && !lowercaseconfirm && !numbersconfirm) {
-    alert("Please select at least one character type")
-
+    var numbersconfirm = confirm("Do you want to allow numbers?");
+    if (!symbolconfirm && !uppercaseconfirm && !lowercaseconfirm && !numbersconfirm) {
+      alert("Please select at least one character type")
+    }
     var symbolconfirm = confirm("Do you want to allow special characters?");
 
     var uppercaseconfirm = confirm("Do you want to allow uppercase letters?");
